2aa349d5c1da7ddceece1015093235005e3b3223
WillCZhang <will.chi.zhang@gmail.com>
2019-09-22 11:01:43 -0700

    added coverage checking

--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
 
+if [ "$(which coverage)" -e "" ]; then
+  echo "Please install pip coverage first"
+  pip3 install coverage
+fi
+
 echo "-------- Executing Tests Before Commit --------"
 echo ""
 echo "-------- Tokenizer Tests --------"
-python3 -m dsl.tokenizer.test.TokenizerSpec
+coverage run -m dsl.tokenizer.test.TokenizerSpec
 echo ""
 echo "-------- Parser Tests --------"
-python3 -m dsl.parser.test.ASTNodeSpec
-python3 -m dsl.parser.test.ConstructorNodeSpec
+coverage run -m dsl.parser.test.ASTNodeSpec
+coverage run -m dsl.parser.test.ConstructorNodeSpec
 
+coverage report
+TEMP=$(coverage report | grep "TOTAL" | awk '{print $4}')
+COVERAGE=$(echo "${TEMP/\%/$NON_EXIST}")
+CODE=$(test $COVERAGE -ge 80)
+exit $CODE
