d372648d69c7f298c5cc4fe4feb25bac36faeda4
raymond.chen <raymondyao1997@gmail.com>
2019-09-26 19:22:43 -0700

    added node modules so that we can build Ant Design React code back to native js code. I also added Webpack to simplify compiling/bundling

---
+++ build/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './Page.jsx',
+    output: {
+      path: __dirname + '/target',
+      publicPath: '/build',
+      filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'less-loader', options: {javascriptEnabled: true}}
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+      ]
+    },
+};
