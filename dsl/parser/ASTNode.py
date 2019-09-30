e940e918ae2de704bda9080d3cdd24a1c50a6cf4
y396920969 <wangyan951027@163.com>
2019-09-29 23:42:52 -0700

    Partial type check done

--- dsl/parser/ASTNode.py
+++ dsl/parser/ASTNode.py
@@ -21,6 +21,24 @@ class ASTNode:
     """
 
     names = []
+    constructor_def = {
+        "Nav": [
+            {"name": "size", "expected_type": "number", "default": 100},
+            {"name": "colour", "expected_type": "string", "default": "black"},
+            {}
+        ],
+        "Header": []
+    }
+
+    default_nav_attr = {'size': 'small', 'align': 'left', 'background': '#D4F2FC'}
+    default_header_attr = {'title': 'Header', 'background': 'black'}
+    default_content_attr = {}
+    default_link_attr = {}
+    default_image_attr = {}
+    default_video_attr = {}
+    default_footer_attr = {}
+    default_button_attr = {}
+    default_page_attr = {}
 
     def __init__(self, list_of_tokens):
         self.tokens = list_of_tokens
@@ -76,6 +94,12 @@ class ProgramNode(ASTNode):
         for l in self.layouts:
             l.name_check()
 
+    def type_check(self):
+        for a in self.assignments:
+            a.type_check()
+        for l in self.layouts:
+            l.type_check()
+
 
 class AssignmentNode(ASTNode):
     STRING = "STRING"
@@ -95,6 +119,7 @@ class AssignmentNode(ASTNode):
         self.var_name = None
         self.assigned = None
         self.assignment_type = None
+        self.tk = None
 
     def parse(self):
         if len(self.tokens) < 3:
@@ -110,18 +135,18 @@ class AssignmentNode(ASTNode):
             raise ParseError(
                 "reach the end of input unexpectedly, not a valid assignment statement")
 
-        tk = self.peek()
-        if tk.is_a(Type.RESERVED):
+        self.tk = self.peek()
+        if self.tk.is_a(Type.RESERVED):
             self.assignment_type = self.FUNC
             self.assigned = ConstructorNode(self.tokens)
             self.assigned.parse()
-        elif tk.is_a(Type.STRING) or tk.is_a(Type.NUMBER) or tk.is_a(Type.VARIABLE):
-            self.assignment_type = self.TYPE_CONVERT_MAP[tk.type]
-            self.assigned = tk.value
+        elif self.tk.is_a(Type.STRING) or self.tk.is_a(Type.NUMBER) or self.tk.is_a(Type.VARIABLE):
+            self.assignment_type = self.TYPE_CONVERT_MAP[self.tk.type]
+            self.assigned = self.tk.value
             self.next()
         else:
             raise ParseError(
-                f"unexpected Token {repr(tk)}: expected a string, number or constructor as an assigned value")
+                f"unexpected Token {repr(self.tk)}: expected a string, number or constructor as an assigned value")
 
     def name_check(self):
         ASTNode.names.append(self.var_name)
@@ -130,11 +155,16 @@ class AssignmentNode(ASTNode):
         if self.assignment_type == self.FUNC:
             self.assigned.name_check()
 
+    def type_check(self):
+        if self.assignment_type == self.FUNC:
+            self.assigned.type_check(self.tk)
+
 
 class ConstructorNode(ASTNode):
     def __init__(self, list_of_tokens):
         super().__init__(list_of_tokens)
         self.params = []
+        self.attr = {}
 
     def parse(self):
         if len(self.tokens) < 3 or Bracket("(") not in self.tokens or Bracket(")") not in self.tokens:
@@ -142,7 +172,7 @@ class ConstructorNode(ASTNode):
                 "unexpected line end: constructor declaration must be in this form: Constructor()")
         if self.tokens.count(Bracket("(")) != self.tokens.count(Bracket(")")):
             raise ParseError(
-                "unexpected line end: number of opening and closing bracket does not mutch")
+                "unexpected line end: number of opening and closing bracket does not match")
         # I'll use a copy of self.token to create assignment node
         # then use "move" to pop that much of tokens from self.token
         move = 3
@@ -176,6 +206,56 @@ class ConstructorNode(ASTNode):
         for param in self.params:
             param.name_check()
 
+    def type_check(self, tk):
+        if self.params:
+            if tk.value == 'Nav':
+                attr = self.default_nav_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Nav component")
+            elif tk.value == 'Header':
+                attr = self.default_header_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Header component")
+            elif tk.value == 'Content':
+                attr = self.default_content_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Content component")
+            elif tk.value == 'Link':
+                attr = self.default_link_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Link component")
+            elif tk.value == 'Image':
+                attr = self.default_image_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Image component")
+            elif tk.value == 'Video':
+                attr = self.default_video_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Video component")
+            elif tk.value == 'Footer':
+                attr = self.default_footer_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Footer component")
+            elif tk.value == 'Button':
+                attr = self.default_button_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Button component")
+            elif tk.value == 'Page':
+                attr = self.default_page_attr
+                for param in self.params:
+                    if param.var_name not in attr:
+                        raise TypeCheckError(f"There is no such attribute called {param.var_name} in Page component")
+            else:
+                raise TypeCheckError(f"No such constructor called {tk.value}")
+
 
 class LayoutNode(ASTNode):
 
@@ -195,3 +275,8 @@ class NameCheckError(Exception):
     def __init__(self, message):
         message = "You didn't declare : " + message
         super().__init__(message)
+
+
+class TypeCheckError(Exception):
+    def __init__(self, message):
+        self.message = message
